4159537eea9f4733595c6ae00f06b339e24f47c3
Tarik012 <tc78200@gmail.com>
2022-04-07 11:47:00 +0200

    Ajout classe Voiture

---
+++ Voiture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitHubVisual
+{
+    internal class Voiture
+    {
+        public int numeroAjoutPageTuto { get; set; }
+    }
+}
